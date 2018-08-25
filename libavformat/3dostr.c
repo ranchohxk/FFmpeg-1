169392e5d20abfdf0286d8c0721f959cd8b29dd7
Huang Xiankui <hxk@dw.gdbbk.com>
2018-08-25 14:22:00 +0800

    test chinese

--- libavformat/3dostr.c
+++ libavformat/3dostr.c
@@ -18,7 +18,7 @@
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
+//黄先奎
 #include "avformat.h"
 #include "internal.h"
 
