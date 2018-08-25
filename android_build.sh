f541f3324ccd27833966a81f487bbb75c53a989f
Huang Xiankui <hxk@dw.gdbbk.com>
2018-08-25 14:55:18 +0800

    add android-build.sh

---
+++ android_build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+make clean
+#这里修改为你的ndk的路径
+export NDK=/data/hxk/software/android-ndk-r10e
+#注意android-9文件夹的版本号，替换好自己的版本号。下面的路径同理
+export SYSROOT=$NDK/platforms/android-9/arch-arm/
+export TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+#编译出来后支持的格式
+export CPU=arm
+export PREFIX=$(pwd)/android/$CPU
+export ADDI_CFLAGS="-marm"
+./configure --target-os=linux \
+--prefix=$PREFIX --arch=arm \
+--disable-doc \
+--enable-shared \
+--disable-static \
+--disable-yasm \
+--disable-symver \
+--enable-gpl \
+--disable-x86asm \
+--disable-ffmpeg \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-ffserver \
+--disable-linux-perf \ 
+--disable-doc \
+--disable-symver \
+--cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+--enable-cross-compile \
+--sysroot=$SYSROOT \
+--extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+--extra-ldflags="$ADDI_LDFLAGS" \
+$ADDITIONAL_CONFIGURE_FLAG
+#make clean
+make
